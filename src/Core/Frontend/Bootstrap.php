8c6551b52d45ebe2137057cf9e8d68aa3d8d7dce
Simon Asika <asika32764@gmail.com>
2015-07-29 16:28:10 +0800

    Add Messages type constant. Fix #78

---
+++ src/Core/Frontend/Bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Part of starter project.
+ *
+ * @copyright  Copyright (C) 2015 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace Windwalker\Core\Frontend;
+
+/**
+ * The Bootstrap class.
+ *
+ * @since  2.0.9
+ */
+class Bootstrap
+{
+	const MSG_SUCCESS = 'success';
+	const MSG_INFO    = 'info';
+	const MSG_WARNING = 'warning';
+	const MSG_DANGER  = 'danger';
+}
