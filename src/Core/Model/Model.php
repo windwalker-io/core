695e77c69ee6aa9c022ee5a5173e5c6218793cc0
Simon Asika <asika32764@gmail.com>
2015-07-29 17:35:29 +0800

    Update file headers

--- src/Core/Model/Model.php
+++ src/Core/Model/Model.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Part of auth project. 
+ * Part of Windwalker project.
  *
  * @copyright  Copyright (C) 2014 - 2015 LYRASOFT Taiwan, Inc. All rights reserved.
  * @license    GNU Lesser General Public License version 3 or later. see LICENSE
