a5a93617b55552b281365338d963a17f43b5f718
Simon Asika <asika32764@gmail.com>
2015-08-14 18:46:47 +0800

    Prepare logger

---
+++ src/Core/Logger/LoggerFactory.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Part of starter project.
+ *
+ * @copyright  Copyright (C) 2015 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace Windwalker\Core\Logger;
+
+use Monolog\Logger;
+
+/**
+ * The LoggerFactory class.
+ *
+ * @since  {DEPLOY_VERSION}
+ */
+class LoggerFactory
+{
+	/**
+	 * Property loggers.
+	 *
+	 * @var  Logger[]
+	 */
+	protected $loggers = array();
+
+	public function createLogger($name, $handlers)
+	{
+
+	}
+}
