23b22d1ef458885907da5bf4a81b660102b09a91
Simon Asika <asika32764@gmail.com>
2016-03-20 01:04:27 +0800

    Add AbstractDataMapperProxy

---
+++ src/Core/Authentication/NullUserHandler.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Part of phoenix project.
+ *
+ * @copyright  Copyright (C) 2016 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+namespace Windwalker\Core\Authentication;
+
+/**
+ * The NullUserHandler class.
+ *
+ * @since  2.1.7.1
+ */
+class NullUserHandler implements UserHandlerInterface
+{
+	/**
+	 * load
+	 *
+	 * @param array $conditions
+	 *
+	 * @return  mixed|false
+	 */
+	public function load($conditions)
+	{
+		return new UserData;
+	}
+
+	/**
+	 * save
+	 *
+	 * @param UserDataInterface $user
+	 *
+	 * @return  UserDataInterface
+	 */
+	public function save(UserDataInterface $user)
+	{
+		return new UserData;
+	}
+
+	/**
+	 * delete
+	 *
+	 * @param UserDataInterface $user
+	 *
+	 * @return  boolean
+	 */
+	public function delete(UserDataInterface $user)
+	{
+		return true;
+	}
+
+	/**
+	 * login
+	 *
+	 * @param UserDataInterface $user
+	 *
+	 * @return  boolean
+	 */
+	public function login(UserDataInterface $user)
+	{
+		return false;
+	}
+
+	/**
+	 * logout
+	 *
+	 * @param UserDataInterface $user
+	 *
+	 * @return bool
+	 */
+	public function logout(UserDataInterface $user)
+	{
+		return true;
+	}
+}
