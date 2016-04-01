18c84b0f269bbc39953c7159406ca01baad71629
Simon Asika <asika32764@gmail.com>
2016-04-01 15:46:28 +0800

    Update Logger.php

--- src/Core/Logger/Logger.php
+++ src/Core/Logger/Logger.php
@@ -35,6 +35,56 @@ use Windwalker\Core\Facade\AbstractProxyFacade;
 class Logger extends AbstractProxyFacade
 {
 	/**
+	 * Detailed debug information
+	 */
+	const DEBUG = 100;
+
+	/**
+	 * Interesting events
+	 *
+	 * Examples: User logs in, SQL logs.
+	 */
+	const INFO = 200;
+
+	/**
+	 * Uncommon events
+	 */
+	const NOTICE = 250;
+
+	/**
+	 * Exceptional occurrences that are not errors
+	 *
+	 * Examples: Use of deprecated APIs, poor use of an API,
+	 * undesirable things that are not necessarily wrong.
+	 */
+	const WARNING = 300;
+
+	/**
+	 * Runtime errors
+	 */
+	const ERROR = 400;
+
+	/**
+	 * Critical conditions
+	 *
+	 * Example: Application component unavailable, unexpected exception.
+	 */
+	const CRITICAL = 500;
+
+	/**
+	 * Action must be taken immediately
+	 *
+	 * Example: Entire website down, database unavailable, etc.
+	 * This should trigger the SMS alerts and wake you up.
+	 */
+	const ALERT = 550;
+
+	/**
+	 * Urgent alert.
+	 */
+	const EMERGENCY = 600;
+	
+	/**
 	 * Property _key.
 	 *
 	 * @var  string
