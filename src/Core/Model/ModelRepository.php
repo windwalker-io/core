3c4675d7f79d76665263898f084c99661862c2fb
Simon Asika <asika32764@gmail.com>
2016-06-30 15:24:40 +0800

    Add ModelRepository

---
+++ src/Core/Model/ModelRepository.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Part of phoenix project.
+ *
+ * @copyright  Copyright (C) 2016 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+namespace Windwalker\Core\Model;
+
+use Windwalker\Core\Model\Traits\ModelRepositoryTrait;
+
+/**
+ * The ModelRepository class.
+ *
+ * @since  {DEPLOY_VERSION}
+ */
+class ModelRepository implements ModelRepositoryInterface
+{
+	use ModelRepositoryTrait;
+}
