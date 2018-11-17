125d2008ab8c3f31b43308c67986ef6b6f7bed10
Simon Asika <asika32764@gmail.com>
2018-11-17 13:55:10 +0800

    All use Translator #482

--- src/Core/Language/Translator.php
+++ src/Core/Language/Translator.php
@@ -9,6 +9,14 @@
 namespace Windwalker\Core\Language;
 
 use Windwalker\Core\Facade\AbstractProxyFacade;
+use Windwalker\Core\Ioc;
+use Windwalker\Core\Package\AbstractPackage;
+use Windwalker\Core\Package\PackageHelper;
+use Windwalker\Filesystem\File;
+use Windwalker\Filesystem\Folder;
+use Windwalker\Language\LanguageNormalize;
+use Windwalker\Structure\Structure;
+use Windwalker\Structure\StructureHelper;
 
 /**
  * The Translator class.
@@ -23,7 +31,7 @@ use Windwalker\Core\Facade\AbstractProxyFacade;
  * @method  static CoreLanguage  load(string $file, string $format = 'ini', string $loader = 'file')
  * @method  static CoreLanguage  addString(string $key, string $string)
  * @method  static CoreLanguage  addStrings(array $strings)
- * @method  static string    getOrphans()
+ * @method  static array     getOrphans()
  * @method  static string    setTraceLevelOffset($level)
  * @method  static string    getTraceLevelOffset()
  * @method  static string    getLocale()
@@ -43,4 +51,201 @@ abstract class Translator extends AbstractProxyFacade
     */
     protected static $_key = 'language';
     // phpcs:enable
+
+    /**
+     * loadAll
+     *
+     * @param string|AbstractPackage $package
+     * @param string                 $format
+     *
+     * @return  void
+     * @throws \ReflectionException
+     */
+    public static function loadAll($package, $format = 'ini')
+    {
+        if (is_string($package)) {
+            $package = PackageHelper::getPackage($package);
+        }
+
+        if (!$package) {
+            return;
+        }
+
+        $path = $package->getDir() . '/Resources/language';
+
+        static::loadAllFromPath($path, $format, $package);
+    }
+
+    /**
+     * loadAllFromPath
+     *
+     * @param   string $path
+     * @param   string $format
+     * @param   string $package
+     */
+    public static function loadAllFromPath($path, $format, $package = null)
+    {
+        $config = Ioc::getConfig();
+
+        $locale  = $config['language.locale'] ?: 'en-GB';
+        $default = $config['language.default'] ?: 'en-GB';
+        $locale  = LanguageNormalize::toLanguageTag($locale);
+        $default = LanguageNormalize::toLanguageTag($default);
+
+        $localePath = $path . '/' . $locale;
+
+        $files = [];
+
+        if (is_dir($localePath)) {
+            $files = array_merge($files, (array) Folder::files($localePath, false, Folder::PATH_BASENAME));
+        }
+
+        $defaultPath = $path . '/' . $default;
+
+        if (is_dir($defaultPath)) {
+            $files = array_merge($files, (array) Folder::files($defaultPath, false, Folder::PATH_BASENAME));
+        }
+
+        foreach ($files as $file) {
+            $ext = File::getExtension($file);
+
+            if (strcasecmp($ext, $format) !== 0) {
+                continue;
+            }
+
+            static::loadFile(File::stripExtension($file), strtolower($format), $package);
+        }
+    }
+
+    /**
+     * getOrphans
+     *
+     * @param boolean $flatten
+     * @param int     $stripPrefix
+     *
+     * @return array
+     */
+    public static function getAutoHandledOrphans($flatten = true, $stripPrefix = 1)
+    {
+        $orphans = static::getOrphans();
+
+        foreach ($orphans as $key => $value) {
+            $value = explode('.', $key);
+
+            $value = array_map('ucfirst', $value);
+
+            foreach (range(1, $stripPrefix) as $i) {
+                array_shift($value);
+            }
+
+            $value = implode(' ', $value);
+
+            $orphans[$key] = $value;
+        }
+
+        if (!$flatten) {
+            $reg = new Structure();
+
+            foreach ($orphans as $key => $value) {
+                $reg->set($key, $value);
+            }
+
+            $orphans = $reg->toArray();
+        }
+
+        return $orphans;
+    }
+
+    /**
+     * getFormattedOrphans
+     *
+     * @param string $format
+     *
+     * @return  string
+     */
+    public static function getFormattedOrphans($format = 'ini')
+    {
+        $formatter = StructureHelper::getFormatClass($format);
+
+        $returns = [];
+        $options = [];
+
+        switch (strtolower($format)) {
+            case 'ini':
+                $orphans = static::getAutoHandledOrphans();
+
+                foreach ($orphans as $key => $value) {
+                    $key2 = explode('.', $key);
+
+                    if (isset($key2[1])) {
+                        $returns[$key2[1]][$key] = $value;
+
+                        continue;
+                    }
+
+                    $returns[$key] = $value;
+                }
+
+                break;
+
+            case 'yaml':
+            case 'yml':
+                $options['inline'] = 99;
+            // No break
+            default:
+                $orphans = static::getAutoHandledOrphans(false);
+                $returns = $orphans;
+        }
+
+        return $formatter::structToString($returns, $options);
+    }
+
+    /**
+     * dumpOrphans
+     *
+     * @param string $format
+     *
+     * @return  void
+     *
+     * @throws \Windwalker\Filesystem\Exception\FilesystemException
+     */
+    public static function dumpOrphans($format = 'ini')
+    {
+        $format = strtolower($format);
+        $ext    = ($format === 'yaml') ? 'yml' : $format;
+
+        $file = WINDWALKER_LOGS . '/language-orphans.' . $ext;
+
+        if (!is_file($file)) {
+            Folder::create(dirname($file));
+            file_put_contents($file, '');
+        }
+
+        $orphans = new Structure();
+        $orphans->loadFile($file, $format, ['processSections' => true]);
+
+        $orphans->loadString(static::getFormattedOrphans($format), $format, ['processSections' => true]);
+
+        file_put_contents($file, $orphans->toString($format, ['inline' => 99]));
+    }
+
+    /**
+     * getFormatter
+     *
+     * @param   string $format
+     *
+     * @return  string
+     *
+     * @throws \DomainException
+     */
+    public static function getFormatter($format)
+    {
+        $class = sprintf('Windwalker\Registry\Format\%sFormat', ucfirst($format));
+
+        if (class_exists($class)) {
+            return $class;
+        }
+
+        throw new \DomainException(sprintf('Class: %s not exists', $class));
+    }
 }
