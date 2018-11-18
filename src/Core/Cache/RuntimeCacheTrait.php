66fb56e1c6fff373b55058cf09fc7cb0d01b1bfa
Simon Asika <asika32764@gmail.com>
2018-11-18 15:46:01 +0800

    Modify runtime cache

--- src/Core/Cache/RuntimeCacheTrait.php
+++ src/Core/Cache/RuntimeCacheTrait.php
@@ -113,12 +113,20 @@ trait RuntimeCacheTrait
      *
      * @param string   $id
      * @param callable $closure
+     * @param bool     $refresh
      *
      * @return  mixed
      * @throws \Psr\Cache\InvalidArgumentException
      */
-    protected function fetch($id, $closure)
+    protected function fetch($id, $closure, $refresh = false)
     {
-        return $this->getCacheInstance()->call($this->getCacheId($id), $closure);
+        $key = $this->getCacheId($id);
+        $cache = $this->getCacheInstance();
+
+        if ($refresh) {
+            $cache->remove($key);
+        }
+
+        return $cache->call($key, $closure);
     }
 }
