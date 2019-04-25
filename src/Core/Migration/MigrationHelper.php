e2ecb127ddc4be2a0b91f98c2777b5d251bce7b2
Simon Asika <asika32764@gmail.com>
2019-04-26 00:26:23 +0800

    Migration env notice use set in windows #558

--- src/Core/Migration/MigrationHelper.php
+++ src/Core/Migration/MigrationHelper.php
@@ -30,7 +30,7 @@ class MigrationHelper
     public static function getEnvCmd(string $env = 'WINDWALKER_MODE', string $value = 'dev'): string
     {
         $prefix = Ioc::getEnvironment()->getPlatform()->isWin()
-            ? 'setenv'
+            ? 'set'
             : 'export';
 
         return sprintf('%s %s=%s', $prefix, $env, $value);
