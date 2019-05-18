998803198bb238976de7ba57ea25827e4aecfee2
Simon Asika <asika32764@gmail.com>
2019-05-18 17:58:34 +0800

    RendererHelper must has addGlobal() and some missing methods #560

--- src/Core/Renderer/RendererHelper.php
+++ src/Core/Renderer/RendererHelper.php
@@ -9,7 +9,9 @@
 namespace Windwalker\Core\Renderer;
 
 use Windwalker\Core\Facade\AbstractProxyFacade;
+use Windwalker\Core\View\Helper\AbstractHelper;
 use Windwalker\Renderer\AbstractRenderer;
+use Windwalker\Renderer\MustacheRenderer;
 use Windwalker\Utilities\Queue\PriorityQueue;
 
 /**
@@ -17,32 +19,39 @@ use Windwalker\Utilities\Queue\PriorityQueue;
  *
  * @see    RendererManager
  *
- * @method  static AbstractRenderer|CoreRendererInterface  getRenderer($type = RendererManager::PHP, $config = [])
- * @method  static PhpRenderer      getPhpRenderer($config = [])
- * @method  static BladeRenderer    getBladeRenderer($config = [])
- * @method  static EdgeRenderer     getEdgeRenderer($config = [])
- * @method  static TwigRenderer     getTwigRenderer($config = [])
- * @method  static PriorityQueue    getGlobalPaths()
- * @method  static RendererManager  addGlobalPath($path, $priority = PriorityQueue::LOW)
- * @method  static RendererManager  addPath($path, $priority = PriorityQueue::LOW)
- * @method  static PriorityQueue    getPaths()
- * @method  static array            getGlobals()
- * @method  static RendererManager  reset()
- * @method  static RendererManager  setPaths($paths)
+ * @method static  AbstractRenderer  getRenderer($type = 'php', $config = [])
+ * @method static  PhpRenderer      getPhpRenderer($config = [])
+ * @method static  BladeRenderer    getBladeRenderer($config = [])
+ * @method static  EdgeRenderer     getEdgeRenderer($config = [])
+ * @method static  TwigRenderer     getTwigRenderer($config = [])
+ * @method static  MustacheRenderer  getMustacheRenderer($config = [])
+ * @method static  PriorityQueue    getGlobalPaths()
+ * @method static  RendererManager  addGlobalPath($path, $priority = PriorityQueue::LOW)
+ * @method static  RendererManager  addPath($path, $priority = PriorityQueue::LOW)
+ * @method static  RendererManager  reset()
+ * @method static  RendererManager  setPaths($paths)
+ * @method static  RendererManager  addHelper($name, $helper)
+ * @method static  AbstractHelper   getHelper($name)
+ * @method static  void             getHelpers()
+ * @method static  RendererManager  setHelpers($helpers)
+ * @method static  RendererManager  addGlobal($name, $value)
+ * @method static  RendererManager  removeGlobal($name)
+ * @method static  array            getGlobals()
+ * @method static  RendererManager  setGlobals($globals)
  *
  * @since  2.0
  */
 abstract class RendererHelper extends AbstractProxyFacade
 {
-    const PHP = 'php';
+    public const PHP = 'php';
 
-    const BLADE = 'blade';
+    public const BLADE = 'blade';
 
-    const EDGE = 'edge';
+    public const EDGE = 'edge';
 
-    const TWIG = 'twig';
+    public const TWIG = 'twig';
 
-    const MUSTACHE = 'mustache';
+    public const MUSTACHE = 'mustache';
 
     protected static $_key = 'renderer.manager';
 
