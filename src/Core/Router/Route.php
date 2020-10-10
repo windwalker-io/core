ebb0a6cdd4aa39fd6aa7395850f785de3dc6207a
Simon Asika <asika32764@gmail.com>
2020-10-10 17:06:27 +0800

    route wip

---
+++ src/Core/Router/Route.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Part of starter project.
+ *
+ * @copyright  Copyright (C) 2020 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+namespace Windwalker\Core\Router;
+
+/**
+ * The Route class.
+ */
+class Route
+{
+    protected array $methods = [];
+
+    protected string $pattern = '';
+}
